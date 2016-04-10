6fa46beb10308ef609dd19fab81fb4d5faa5c922
Regis <boudinot.regis@yahoo.com>
2016-04-10 07:11:03 +0000

    can query table objects and pull objects from id

--- re.js
+++ re.js
@@ -14,21 +14,29 @@ class Rejs {
       })
     }
   }
+  
+  getTable(table) {
+    return JSON.parse(fs.readFileSync(`./rejs/${table}`, 'utf8'))
+  }
+  
+  findId(table, id) {
+    let tableQuery = JSON.parse(fs.readFileSync(`./rejs/${table}`, 'utf8'))
+    return tableQuery[id]
+  }
 
   createTable(tableName) {
-    if(!fs.existsSync(`./rejs/${tableName}.txt`)) {
-      let tableInit = `${tableName}`
+    if(!fs.existsSync(`./rejs/${tableName}`)) {
       let InitialData = {"0": {"table": `${tableName}`}}
-      fs.writeFileSync(`./rejs/${tableName}.txt`, JSON.stringify(InitialData))
+      fs.writeFileSync(`./rejs/${tableName}`, JSON.stringify(InitialData))
     }
   }
 
   writeToTable(table, data) {
-    fs.writeFileSync(`./rejs/${table}.txt`, JSON.stringify(data))
+    fs.writeFileSync(`./rejs/${table}`, JSON.stringify(data))
   }
 
   newData(table, data) {
-    let tableRead = JSON.parse(fs.readFileSync(`./rejs/${table}.txt`, 'utf8'))
+    let tableRead = JSON.parse(fs.readFileSync(`./rejs/${table}`, 'utf8'))
     let lastId = Object.keys(tableRead)
     lastId = parseInt((lastId[lastId.length - 1])) + 1
     tableRead[lastId] = data
@@ -36,7 +44,7 @@ class Rejs {
   }
 
   deleteById(table, id) {
-    let tableRead = fs.readFileSync(`./rejs/${table}.txt`, 'utf8')
+    let tableRead = fs.readFileSync(`./rejs/${table}`, 'utf8')
     let modData = JSON.parse(tableRead)
     delete modData[`${id}`]
     this.writeToTable(`${table}`, modData)
