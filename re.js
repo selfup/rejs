74753985d1a1c6212c2b52d8703a8035c6338fe5
Regis <boudinot.regis@yahoo.com>
2020-06-08 18:17:23 -0500

    prettier - update deps

--- re.js
+++ re.js
@@ -1,100 +1,119 @@
-'use strict'
+const fs = require('fs');
+const { values, filter, includes } = require('lodash');
 
-const fs = require('fs')
-const _  = require('lodash')
-
-const _resetTable   = Symbol('resetTable')
-const _modifyTable  = Symbol('modifyTable')
-const _replaceTable = Symbol('replaceTable')
-const _initialData  = Symbol('initialData')
-const _multiCall    = Symbol('multiCall')
+const _resetTable = Symbol('resetTable');
+const _modifyTable = Symbol('modifyTable');
+const _replaceTable = Symbol('replaceTable');
+const _initialData = Symbol('initialData');
+const _multiCall = Symbol('multiCall');
 
 class Rejs {
   constructor() {
-    if (fs.existsSync("selfup-rejs")) return
-    fs.mkdirSync("selfup-rejs")
+    if (fs.existsSync('selfup-rejs')) {
+      return null;
+    }
+
+    fs.mkdirSync('selfup-rejs');
+
+    return null;
   }
 
-  // public
   createTable(tableName) {
-    if (fs.existsSync(`./selfup-rejs/${tableName}`)) return
-    this[_resetTable](tableName)
+    if (fs.existsSync(`./selfup-rejs/${tableName}`)) {
+      return null;
+    }
+
+    this[_resetTable](tableName);
+
+    return null;
   }
 
   createTables() {
-    return Array.from(arguments).map(table => this.createTable(table))
+    return Array.from(arguments).map((table) => this.createTable(table));
   }
 
   newData(tableName, data) {
-    this[_modifyTable](tableName, t => t[t[0].nextId++] = data)
+    this[_modifyTable](tableName, (t) => (t[t[0].nextId++] = data));
   }
 
   newDatas() {
-    return Array.from(arguments).map(arg => this.newData(arg[0], arg[1]))
+    return Array.from(arguments).map((arg) => this.newData(arg[0], arg[1]));
   }
 
   deleteById(tableName, id) {
-    this[_modifyTable](tableName, t => delete t[id])
+    this[_modifyTable](tableName, (t) => delete t[id]);
   }
 
   dropTable(tableName) {
-    fs.unlinkSync(`./selfup-rejs/${tableName}`)
+    const table = `./selfup-rejs/${tableName}`;
+
+    fs.unlinkSync(table);
   }
 
   dropTables() {
-    return this[_multiCall](Array.from(arguments), this.dropTable)
+    return this[_multiCall](Array.from(arguments), this.dropTable);
   }
 
   updateTable(tableName, data) {
-    this[_resetTable](tableName)
-    this.newData(tableName, data)
+    this[_resetTable](tableName);
+
+    this.newData(tableName, data);
   }
 
   updateTables() {
-    return Array.from(arguments).map(arg => this.updateTable(arg[0], arg[1]))
+    return Array.from(arguments).map((arg) => this.updateTable(arg[0], arg[1]));
   }
 
   getTable(tableName) {
-    return JSON.parse(fs.readFileSync(`./selfup-rejs/${tableName}`, 'utf8'))
+    const file = fs.readFileSync(`./selfup-rejs/${tableName}`, 'utf-8');
+
+    return JSON.parse(file);
   }
 
   getTables() {
-    return this[_multiCall](Array.from(arguments), this.getTable)
+    return this[_multiCall](Array.from(arguments), this.getTable);
   }
 
   findId(tableName, id) {
-    return this.getTable(tableName)[id]
+    return this.getTable(tableName)[id];
   }
 
   where(tableName, prop) {
-    const whereTable = this.getTable(tableName)
-    const records    = _.values(whereTable)
-    records.shift() // remove the metadata
-    return _.filter(records, (record) => _.includes(record, prop))
+    const whereTable = this.getTable(tableName);
+    const records = values(whereTable);
+
+    records.shift();
+
+    return filter(records, (record) => includes(record, prop));
   }
 
-  // private
   [_multiCall](args, fn) {
-    return args.map(table => fn(table))
+    return args.map((table) => fn(table));
   }
 
   [_replaceTable](tableName, data) {
-    fs.writeFileSync(`./selfup-rejs/${tableName}`, JSON.stringify(data))
+    const fileName = `./selfup-rejs/${tableName}`;
+
+    fs.writeFileSync(fileName, JSON.stringify(data));
   }
 
   [_resetTable](tableName) {
-    this[_replaceTable](tableName, this[_initialData](tableName))
+    this[_replaceTable](tableName, this[_initialData](tableName));
   }
 
   [_modifyTable](tableName, fn) {
-    const table = this.getTable(tableName)
-    fn(table)
-    this[_replaceTable](tableName, table)
+    const table = this.getTable(tableName);
+
+    fn(table);
+
+    this[_replaceTable](tableName, table);
   }
 
-  [_initialData](tableName) {
-    return {"0": {"table": tableName, nextId: 1}}
+  [_initialData](table) {
+    const nextId = 1;
+
+    return { '0': { table, nextId } };
   }
 }
 
-module.exports = Rejs
+module.exports = Rejs;
--- test/integration_test.js
+++ test/integration_test.js
@@ -1,180 +1,177 @@
-'use strict'
+'use strict';
 
-const assert = require('chai').assert
-const Selfup = require('../re.js')
+const assert = require('chai').assert;
+const Selfup = require('../re.js');
 
-describe('Rejs', function() {
-  beforeEach(function() {
-    this.rejs = new Selfup
-    this.rejs.createTable('testOne')
-  })
+describe('Rejs', function () {
+  beforeEach(function () {
+    this.rejs = new Selfup();
+    this.rejs.createTable('testOne');
+  });
 
-  afterEach(function() {
-    this.rejs.dropTable('testOne')
-  })
+  afterEach(function () {
+    this.rejs.dropTable('testOne');
+  });
 
-  describe('getTable and newData', function() {
-    it('appends new data and returns all the appended data', function() {
-      this.rejs.newData('testOne', {test: "test data 1"})
-      this.rejs.newData('testOne', {test: "test data 2"})
+  describe('getTable and newData', function () {
+    it('appends new data and returns all the appended data', function () {
+      this.rejs.newData('testOne', { test: 'test data 1' });
+      this.rejs.newData('testOne', { test: 'test data 2' });
 
       const expected = {
         '0': { table: 'testOne', nextId: 3 },
         '1': { test: 'test data 1' },
         '2': { test: 'test data 2' },
-      }
+      };
 
-      assert.deepEqual(expected, this.rejs.getTable('testOne'))
-    })
-  })
+      assert.deepEqual(expected, this.rejs.getTable('testOne'));
+    });
+  });
 
-   describe('where', function() {
-    it('selects records that have a matching key', function() {
-      this.rejs.newData('testOne', {test: "test data 1"})
-      this.rejs.newData('testOne', {test: "test data 2"})
-      this.rejs.newData('testOne', {test: "test data 1"})
-      this.rejs.newData('testOne', {test: "test data 4"})
+  describe('where', function () {
+    it('selects records that have a matching key', function () {
+      this.rejs.newData('testOne', { test: 'test data 1' });
+      this.rejs.newData('testOne', { test: 'test data 2' });
+      this.rejs.newData('testOne', { test: 'test data 1' });
+      this.rejs.newData('testOne', { test: 'test data 4' });
 
-      const expected = [{test: 'test data 1'}, {test: 'test data 1'}]
-      assert.deepEqual(expected, this.rejs.where('testOne', 'test data 1'))
-    })
-  })
+      const expected = [{ test: 'test data 1' }, { test: 'test data 1' }];
+      assert.deepEqual(expected, this.rejs.where('testOne', 'test data 1'));
+    });
+  });
 
-  describe('findId', function() {
-    it('returns the matching record if one exists', function() {
-      this.rejs.newData('testOne', {test: "test data 1"})
-      this.rejs.newData('testOne', {test: "test data 2"})
+  describe('findId', function () {
+    it('returns the matching record if one exists', function () {
+      this.rejs.newData('testOne', { test: 'test data 1' });
+      this.rejs.newData('testOne', { test: 'test data 2' });
 
-      assert.deepEqual({test: 'test data 1'},
-        this.rejs.findId('testOne', '1')
-      )
-      assert.deepEqual({test: 'test data 2'},
-        this.rejs.findId('testOne', '2')
-      )
-      assert.equal(null,
-        this.rejs.findId('testOne', '3')
-      )
-    })
-  })
+      assert.deepEqual(
+        { test: 'test data 1' },
+        this.rejs.findId('testOne', '1'),
+      );
+      assert.deepEqual(
+        { test: 'test data 2' },
+        this.rejs.findId('testOne', '2'),
+      );
+      assert.equal(null, this.rejs.findId('testOne', '3'));
+    });
+  });
 
-  describe('deleteById', function() {
-    it('deletes the correct object by ID in the correct table', function() {
-      this.rejs.newData('testOne', {test: "test data 1"})
-      this.rejs.newData('testOne', {test: "test data 2"})
-      this.rejs.deleteById('testOne', '2')
-      assert.equal(null, this.rejs.findId('testOne', '2'))
-    })
-  })
+  describe('deleteById', function () {
+    it('deletes the correct object by ID in the correct table', function () {
+      this.rejs.newData('testOne', { test: 'test data 1' });
+      this.rejs.newData('testOne', { test: 'test data 2' });
+      this.rejs.deleteById('testOne', '2');
+      assert.equal(null, this.rejs.findId('testOne', '2'));
+    });
+  });
 
-  describe('updateTable', function() {
-    it('replaces the data in a table', function() {
-      this.rejs.newData('testOne',     {test: "old data"})
-      this.rejs.updateTable('testOne', {test: "new data"})
+  describe('updateTable', function () {
+    it('replaces the data in a table', function () {
+      this.rejs.newData('testOne', { test: 'old data' });
+      this.rejs.updateTable('testOne', { test: 'new data' });
       const expected = {
         '0': { table: 'testOne', nextId: 2 },
         '1': { test: 'new data' },
-      }
-      assert.deepEqual(expected, this.rejs.getTable('testOne'))
-    })
-  })
+      };
+      assert.deepEqual(expected, this.rejs.getTable('testOne'));
+    });
+  });
 
-  describe('getTables: two', function() {
-    it('returns an array of two tables', function() {
-      this.rejs.createTable('firstTable')
-      this.rejs.createTable('secondTable')
-
-      const expected = [
-        {'0': { table: 'firstTable', nextId: 1 }},
-        {'0': { table: 'secondTable', nextId: 1 }}
-      ]
-      const tbls = this.rejs.getTables('firstTable', 'secondTable')
-
-      assert.deepEqual(expected, tbls)
-
-      this.rejs.dropTable('firstTable')
-      this.rejs.dropTable('secondTable')
-    })
-  })
-
-  describe('getTables: three', function() {
-    it('returns an array of four tables', function() {
-      this.rejs.createTable('firstTable')
-      this.rejs.createTable('secondTable')
-      this.rejs.createTable('thirdTable')
-      this.rejs.createTable('fourthTable')
+  describe('getTables: two', function () {
+    it('returns an array of two tables', function () {
+      this.rejs.createTable('firstTable');
+      this.rejs.createTable('secondTable');
 
       const expected = [
-        {'0': { table: 'firstTable', nextId: 1 }},
-        {'0': { table: 'secondTable', nextId: 1 }},
-        {'0': { table: 'thirdTable', nextId: 1 }}
-      ]
+        { '0': { table: 'firstTable', nextId: 1 } },
+        { '0': { table: 'secondTable', nextId: 1 } },
+      ];
+      const tbls = this.rejs.getTables('firstTable', 'secondTable');
+
+      assert.deepEqual(expected, tbls);
+
+      this.rejs.dropTable('firstTable');
+      this.rejs.dropTable('secondTable');
+    });
+  });
+
+  describe('getTables: three', function () {
+    it('returns an array of four tables', function () {
+      this.rejs.createTable('firstTable');
+      this.rejs.createTable('secondTable');
+      this.rejs.createTable('thirdTable');
+      this.rejs.createTable('fourthTable');
+
+      const expected = [
+        { '0': { table: 'firstTable', nextId: 1 } },
+        { '0': { table: 'secondTable', nextId: 1 } },
+        { '0': { table: 'thirdTable', nextId: 1 } },
+      ];
 
       const tbls = this.rejs.getTables(
         'firstTable',
         'secondTable',
-        'thirdTable'
-      )
+        'thirdTable',
+      );
 
-      assert.deepEqual(expected, tbls)
+      assert.deepEqual(expected, tbls);
 
-      this.rejs.dropTable('firstTable')
-      this.rejs.dropTable('secondTable')
-      this.rejs.dropTable('thirdTable')
-    })
-  })
+      this.rejs.dropTable('firstTable');
+      this.rejs.dropTable('secondTable');
+      this.rejs.dropTable('thirdTable');
+    });
+  });
 
-  describe('getTables: four - dropTables: four - createTable: four', function() {
-    it('gets 4 - drops 4 - creates 4: tables', function() {
+  describe('getTables: four - dropTables: four - createTable: four', function () {
+    it('gets 4 - drops 4 - creates 4: tables', function () {
       this.rejs.createTables(
         'firstTable',
         'secondTable',
         'thirdTable',
-        'fourthTable'
-      )
+        'fourthTable',
+      );
 
       const expected = [
-        {'0': { table: 'firstTable', nextId: 1 }},
-        {'0': { table: 'secondTable', nextId: 1 }},
-        {'0': { table: 'thirdTable', nextId: 1 }},
-        {'0': { table: 'fourthTable', nextId: 1 }}
-      ]
+        { '0': { table: 'firstTable', nextId: 1 } },
+        { '0': { table: 'secondTable', nextId: 1 } },
+        { '0': { table: 'thirdTable', nextId: 1 } },
+        { '0': { table: 'fourthTable', nextId: 1 } },
+      ];
 
       const tbls = this.rejs.getTables(
         'firstTable',
         'secondTable',
         'thirdTable',
-        'fourthTable'
-      )
+        'fourthTable',
+      );
 
-      assert.deepEqual(expected, tbls)
+      assert.deepEqual(expected, tbls);
 
       this.rejs.dropTables(
         'firstTable',
         'secondTable',
         'thirdTable',
-        'fourthTable'
-      )
-    })
-  })
+        'fourthTable',
+      );
+    });
+  });
 
-  describe('newDatas and updateTables', function() {
-    it('replaces the data in multiple tables', function() {
-      this.rejs.createTables('firstTable',
-        'secondTable',
-        'thirdTable'
-      )
+  describe('newDatas and updateTables', function () {
+    it('replaces the data in multiple tables', function () {
+      this.rejs.createTables('firstTable', 'secondTable', 'thirdTable');
 
       this.rejs.newDatas(
-        ['firstTable', {test: "old data"}],
-        ['secondTable', {test: "old data"}],
-        ['thirdTable', {test: "old data"}]
-      )
+        ['firstTable', { test: 'old data' }],
+        ['secondTable', { test: 'old data' }],
+        ['thirdTable', { test: 'old data' }],
+      );
 
       this.rejs.updateTables(
-        ['firstTable', {test: "new data"}],
-        ['secondTable', {test: "new data"}],
-        ['thirdTable', {test: "new data"}]
-      )
+        ['firstTable', { test: 'new data' }],
+        ['secondTable', { test: 'new data' }],
+        ['thirdTable', { test: 'new data' }],
+      );
 
       const expected = [
         {
@@ -188,28 +185,26 @@ describe('Rejs', function() {
         {
           '0': { table: 'thirdTable', nextId: 2 },
           '1': { test: 'new data' },
-        }
-      ]
+        },
+      ];
 
-      assert.deepEqual(expected,
-        this.rejs.getTables(
-          'firstTable',
-          'secondTable',
-          'thirdTable')
-        )
+      assert.deepEqual(
+        expected,
+        this.rejs.getTables('firstTable', 'secondTable', 'thirdTable'),
+      );
 
-      this.rejs.dropTables('firstTable', 'secondTable', 'thirdTable')
-    })
-  })
+      this.rejs.dropTables('firstTable', 'secondTable', 'thirdTable');
+    });
+  });
 
-  describe('newAndGetBenchmark', function() {
-    it('can append and fetch a good amount of data', function() {
+  describe('newAndGetBenchmark', function () {
+    it('can append and fetch a good amount of data', function () {
       for (let i = 0; i < 10; i++) {
-        this.rejs.newData('testOne', {test: "test data 2"})
+        this.rejs.newData('testOne', { test: 'test data 2' });
       }
       for (let i = 0; i < 1000; i++) {
-        this.rejs.getTable('testOne')
+        this.rejs.getTable('testOne');
       }
-    })
-  })
-})
+    });
+  });
+});
